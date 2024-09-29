4f3ade1cbeedf02bc6f96eaeff107ab4b67b7a85
KtranH <87740741+KtranH@users.noreply.github.com>
2024-09-29 09:43:36 +0700

    Update front end

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
 // Cấu hình view engine là EJS
 app.set('view engine', 'ejs');
+app.set('layout', 'layout'); 
+app.use(expressLayouts);
 
-// Import các routes
+// Cấu hình file chứa css, js,...
+app.use(express.static('public'));
+
+// Xây dựng route Home
 const homeRoute = require('./routes/home');
-
-// Sử dụng các routes
-app.use('/', homeRoute);      // Route cho trang Home
+app.use('/', homeRoute);   
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Route cho trang Home
 router.get('/', (req, res) => {
-  res.render('home');  // Render file home.ejs
+  res.render('home', { title: 'Trang chủ' });  // Render file home.ejs
 });
 
 module.exports = router;
